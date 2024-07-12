c14a0138900b428316cd3c294a481a476aec2f3d
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2024-07-12 15:08:20 +0600

    add names in countries

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,20 +5,38 @@ import Country from '../Country/Country';
 
 const Countries = () => {
     const [countries,setCountries] = useState([]);
+    const[addNames,setAddNames] = useState([]);
     useEffect( () => {
         fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
 
+
+    const handleAddNames = (names) => {
+        // console.log("This is a handle name");
+        // console.log(names);
+        const newHandleNames = [...addNames,names];
+        setAddNames(newHandleNames);
+        
+    }    
+
     return (
         <div>
             <h2>Countries :- {countries.length}</h2>
+
+            <div>
+                <h3>Countries Details :- {addNames.length}</h3>
+                {
+                    addNames.map(names => <li>{names.name.common}</li>)
+                }
+            </div>
 
             <div className='countries'>
                 {
                     countries.map(country => <Country
                          key={country.cca3}
+                         handleAddNames={handleAddNames}
                           country={country}
                           ></Country>)
                 }
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country,handleAddNames}) => {
     console.log(country);
     const {flags,name,cca3,maps,population,area,region,status} = country;
 
@@ -9,7 +9,13 @@ const Country = ({country}) => {
     const visitedHandle = () => {
         setVisited(!visited);
     }
+
+    // const paramAddNumber = () => {
+    //         handleAddName(country)
+    //     }
     return (
+
+
         <div className={`country ${visited ? 'visit' : 'non-visit'}`}>
             <img src={flags.png} alt="" />
             <h3 style={{color: visited ? 'red' : 'green'}}>Country Name : {name.common}</h3>
@@ -20,9 +26,10 @@ const Country = ({country}) => {
             <p>Region : {region}</p>
             <span>Status : {status}</span><br /><br />
             {/* <p>Languages : {languages.pap}</p> */}
-            <button onClick={visitedHandle}>Visited</button>
+            <button onClick={ () => handleAddNames(country)}>Show Name</button><br /><br />
+            <button onClick={visitedHandle}>{visited ? 'Visited' : 'Going On'}</button>
             {
-                visited ? 'I want to visited' : 'Going on'
+                visited ? 'I have visited this country' : 'I want to visited'
             }
 
         </div>
